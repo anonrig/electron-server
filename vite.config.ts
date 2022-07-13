1c2bad52a5dc5aac972160f677ec79d41d8d1308
Yagiz Nizipli <yagiz@nizipli.com>
2022-07-13 17:43:11 -0400

    feat: add sourcemap support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
       name: "electron-server",
